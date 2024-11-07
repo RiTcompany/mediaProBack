ce4a7df1b96cdaeb3921855c8969d08cc471fe98
Miqael Nazarian <mikanaz1203@gmail.com>
2024-11-08 01:36:20 +0300

    feat: add header content-type/application_json

--- src/main/java/org/example/controllers/MobileFileController.java
+++ src/main/java/org/example/controllers/MobileFileController.java
@@ -2,6 +2,7 @@ package org.example.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.example.services.MobileFileService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,9 @@ public class MobileFileController {
 
     @GetMapping("/.well-known/{file}")
     public ResponseEntity<byte[]> getPracticeTask(@PathVariable("file") String file) throws IOException {
-        return ResponseEntity.ok(mobileFileService.getStaticFile(file));
+        byte[] fileContent = mobileFileService.getStaticFile(file);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fileContent);
     }
 }
